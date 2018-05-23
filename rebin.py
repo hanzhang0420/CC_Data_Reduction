4504146887438f9ac21177f37d83d726a80b37fd
hanzhang0420 <hanzh0420@ufl.edu>
2018-05-23 16:04:01 -0400

    Add files via upload

---
+++ rebin.py
@@ -0,0 +1,8 @@
+import numpy
+def rebin(a, fshape):
+    if len(a.shape)>1:
+        sh = fshape[0],a.shape[0]//fshape[0],fshape[1],a.shape[1]//fshape[1]
+        return a.reshape(sh).mean(-1).mean(1)
+    else:
+        sh = fshape,a.shape[0]//fshape
+        return a.reshape(sh).mean(1)
